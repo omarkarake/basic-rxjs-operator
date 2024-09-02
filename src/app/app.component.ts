0423f2f387899c9cddba7aa7145903cece511bb9
omar karake <omar.karake@amalitechtraining.org>
2024-09-02 11:53:47 +0200

    project initiation and some basic ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, interval, Observable, of, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'exploring-basic-rxjs-operator';
+  numbers$: Observable<number>;
+  colors$: Observable<string>;
+  intervalNumbers$: Observable<number>;
+  newArray: string[] = ['blue', 'yellow', 'green'];
+
+  constructor() {
+    this.numbers$ = of(1, 2, 3, 4, 5);
+    this.colors$ = from(this.newArray);
+    this.intervalNumbers$ = interval(1000);
+  }
+
+  ngOnInit(): void {
+    console.log('----------------------of operator----------------------');
+    this.numbers$.subscribe({
+      next: (value) => console.log('next: ', value),
+      error: (error) => console.log('error: ', error),
+      complete: () => console.log('the end emmition of value to: of operator'),
+    });
+
+    console.log('----------------------from operator--------------------');
+    this.colors$.subscribe({
+      next: (value) => console.log('value: ', value),
+      error: (error) => console.log('error: ', error),
+      complete: () =>
+        console.log('the end of emmition values to: from operator'),
+    });
+
+    console.log('----------------------interval operator---------------------');
+    const takeFiveNumbers$ = this.intervalNumbers$.pipe(take(5));
+    takeFiveNumbers$.subscribe({
+      next: (value) => console.log('value: ', value),
+      error: (error) => console.log('error: ', error),
+      complete: () =>
+        console.log(
+          'the interval that emmit 5 numbers using take is completed'
+        ),
+    });
+  }
+
+  fromOperator() {
+    throw new Error('Method not implemented.');
+  }
+  intervalOperator() {
+    throw new Error('Method not implemented.');
+  }
+  ofOperator() {
+    throw new Error('Method not implemented.');
+  }
 }
