e1c728631cbd5d4800fd82f91307f8b87f11d51e
omar karake <omar.karake@amalitechtraining.org>
2024-09-02 12:59:23 +0200

    working on the concat the observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { from, interval, Observable, of, take } from 'rxjs';
+import { concat, from, interval, Observable, of, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +11,17 @@ export class AppComponent implements OnInit {
   numbers$: Observable<number>;
   colors$: Observable<string>;
   intervalNumbers$: Observable<number>;
+  newArrayNumbers: number[] = [6, 7, 8, 9, 10];
+  numbersFrom6To10$: Observable<number>;
+  concatedNumbers$: Observable<number>;
   newArray: string[] = ['blue', 'yellow', 'green'];
 
   constructor() {
     this.numbers$ = of(1, 2, 3, 4, 5);
     this.colors$ = from(this.newArray);
+    this.numbersFrom6To10$ = from(this.newArrayNumbers);
     this.intervalNumbers$ = interval(1000);
+    this.concatedNumbers$ = concat(of(1, 2, 3, 4, 5), from([6, 7, 8, 9, 10]));
   }
 
   ngOnInit(): void {}
@@ -48,6 +53,17 @@ export class AppComponent implements OnInit {
       next: (value) => console.log('next: ', value),
       error: (error) => console.log('error: ', error),
       complete: () => console.log('the end emmition of value to: of operator'),
+    });
+  }
+
+  concatOperator() {
+    this.concatedNumbers$.subscribe({
+      next: (value) => console.log('value: ', value),
+      error: (error) => console.log('error: ', error),
+      complete: () =>
+        console.log(
+          'the end of the concated map, first observable 1 to 5 and second observable is from 6 to 10'
+        ),
     });
   }
 }
